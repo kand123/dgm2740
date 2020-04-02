e97c4d4d0d5cfb26037ced4ddfe786ba3156de81
Kristin <kbrandtsojo@gmail.com>
2020-04-02 16:08:34 -0600

    added images

---
+++ unit09/js/scripts.js
@@ -0,0 +1,16 @@
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+// $(document).ready(function(){
+//     $('.headSlider').slick({
+//         dots: true,
+//         arrows: false, 
+//         infinite: true, 
+//         speed: 2000,
+//         cssEase: 'ease-out', 
+//         autoplay: true, 
+//         autoplaySpeed: 30000
+//     });
+// });
