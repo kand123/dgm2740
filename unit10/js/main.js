3212c59af2fb1028479199dee5b394c9abeaff03
Kristin <kbrandtsojo@gmail.com>
2020-04-13 17:45:19 -0600

    finished design

--- unit10/js/main.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu () {
-    document.getElementById('hamburgerBtn').classList.toggle('open');
-    document.getElementById('primaryNav').classList.toggle('open');
-}
-
-var x = document.getElementById('hamburgerBtn');
-
-x.onclick = toggleMenu;
